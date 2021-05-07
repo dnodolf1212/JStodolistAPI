c7303b3acae06795f4228387bc9acba30736dede
dnodolf1212 <dnodolf@gmail.com>
2021-05-06 18:12:43 -0700

    fix versioning syntax, uncomment rack0-cors in gemfile. Uncomment rack-cors logic for origin and resources in config/cors

--- app/controllers/api/v1/servers_controller.rb
+++ app/controllers/api/v1/servers_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ServersController < ApplicationController
 
   def index 
     @servers = Server.all
-    render json: @servers
+    render json: @servers, only: [:id, :name], include: :tables
   end
 
 end
