224960069bfc486bb496c6d84265636bb4d0ed26
dnodolf1212 <dnodolf@gmail.com>
2021-05-13 07:13:30 -0700

    build server_controller functionality

--- app/controllers/api/v1/servers_controller.rb
+++ app/controllers/api/v1/servers_controller.rb
@@ -5,4 +5,47 @@ class Api::V1::ServersController < ApplicationController
     render json: @servers, only: [:id, :name], include: :tables
   end
 
+  def new 
+    @server = Server.new
+  end
+
+  def show
+    if @server == nil
+      flash[:alert] = "Please log in"
+        redirect_to servers_path
+      end
+  end
+
+  def create
+    @server = Server.new(server_params)
+    if @server.save
+      redirect_to server_path(@server)
+    end
+  end
+
+  def edit
+    if @server 
+      render :edit 
+    else 
+      redirect_to servers_path
+    end
+  end
+
+  def update
+    @server.update(server_params)
+    redirect_to server_path
+  end
+
+  def destroy 
+    @server.destroy
+    redirect_to servers_path
+  end
+
+  private 
+
+  def server_params 
+    params.require(:server).permit(:name, :password)
+  end
+end
+
 end
